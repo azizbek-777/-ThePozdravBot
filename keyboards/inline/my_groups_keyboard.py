169581ff7452ccc05ff2ee0fa9467a910d40cb71
Azizbek Berdimuratov <a.berdimuratov@uzinfocom.uz>
2024-12-04 19:46:57 +0500

    ok

--- keyboards/inline/my_groups_keyboard.py
+++ keyboards/inline/my_groups_keyboard.py
@@ -7,7 +7,7 @@ async def my_groups_keyboard(groups, dp, locale):
     for id, group_id, _ in groups:
         group = await dp.bot.get_chat(group_id)
         keyboard.add(InlineKeyboardButton(text=group.title, callback_data=f"reminder_my_group_id:{id}"))
-        keyboard.add(
-            InlineKeyboardButton(text=MESSAGES[locale]['back'], callback_data="main_menu"),
-        )
+    keyboard.add(
+        InlineKeyboardButton(text=MESSAGES[locale]['back'], callback_data="main_menu"),
+    )
     return keyboard
