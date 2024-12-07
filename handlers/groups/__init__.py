03a666d6d416f66d2171af8842dd9e6f5158d95a
Azizbek Berdimuratov <a.berdimuratov@uzinfocom.uz>
2024-12-07 21:28:23 +0500

    freat: implemend start command for group and supergroup

--- handlers/groups/__init__.py
+++ handlers/groups/__init__.py
@@ -1,3 +1,4 @@
 from . import list_birthday
 from . import help
+from . import start
 from . import add
--- handlers/groups/start.py
+++ handlers/groups/start.py
@@ -4,10 +4,12 @@ from aiogram.dispatcher import FSMContext
 
 from lang.messages import MESSAGES
 from loader import dp
+from utils.misc.send_add_birthday_message import send_birthday_message
 
 @dp.message_handler(CommandStart(), chat_type=["group", "supergroup"])
-async def bot_help_group(message: Message, state: FSMContext):
+async def bot_start_group(message: Message, state: FSMContext):
     data = await state.get_data()
     locale = data.get("locale", "ru")
-    text = MESSAGES[locale]["commands_list_in_group"]
-    await message.reply(text)
+    bot = await dp.bot.get_me()
+    await send_birthday_message(message, bot.username, locale)
+
