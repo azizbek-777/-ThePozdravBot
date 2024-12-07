7811e3ccf761fc24a23c5e3852a3fd01367c9195
Azizbek Berdimuratov <a.berdimuratov@uzinfocom.uz>
2024-12-07 21:47:22 +0500

    freat: implemend start command for group and supergroup

--- handlers/users/new_chat_member.py
+++ handlers/users/new_chat_member.py
@@ -6,13 +6,13 @@ from utils.misc import send_birthday_message
 
 @dp.message_handler(content_types=["new_chat_members"])
 async def bot_new_chat_member(message: types.Message, state: FSMContext):
-    data = await state.get_data()
-    locale = data.get("locale", "ru")
+    # data = await state.get_data()
+    # locale = data.get("locale", "ru")
     for new_member in message.new_chat_members:
         bot = await dp.bot.get_me()
         if new_member.id == bot.id:
             try:
-                await send_birthday_message(message, bot.username, locale)
+                # await send_birthday_message(message, bot.username, locale)
                 await db.add_group(message.chat.id)
                 break
             except Exception as err:
